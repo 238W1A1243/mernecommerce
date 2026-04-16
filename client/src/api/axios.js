7578f42954a2235547dd1b1ac6016c8eb68527af
238W1A1243 <238w1a1243@vrsec.ac.in>
2026-04-16 16:20:13 +0530

    day 2 admin, add product

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,28 +1,14 @@
-const express= require("express")
-const router=express.Router() 
-const bcrypt=require("bcrypt")
-const jwt= require("jsonwebtoken")
-const User= require("../models/User")
-
-router.post("/register",async (req,res)=>{
-    try{
-        const {name,email,password,mobile}=req.body 
-        const existingUser= await User.findOne({email})
-
-        if(existingUser){
-            return res.status(401).json({message:"User already exists"})
-        }
-        const hashedPassword= await bcrypt.hash(password,10)
-        const user=await User.create({
-            name,email,password:hashedPassword,mobile
-        })
-        return res.status(201).json({message:"User created successfully"})
+import axios from 'axios'
+const API = axios.create({
+    baseURL:"http://localhost:5000/api"
+})
+API.interceptors.request.use((req)=>{
+    const token=localStorage.getItem("token")
+    if(token){
+        req.headers.Authorization=`Bearer ${token}`
     }
-    catch(err){
-        console.log("error from register",err)
-        return res.status(500).json({message:"Error in server"})
-    }
+    return req
 })
 
 
-module.exports=router
+export default API
